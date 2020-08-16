d8d2d8c9f4b45541d833ead48f827456d808a8e7
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-08-16 11:05:14 +0100

    submit form instead of button click simulate

--- src/Components/restaurants/Search/__tests__/Search.test.js
+++ src/Components/restaurants/Search/__tests__/Search.test.js
@@ -12,20 +12,22 @@ const initialState = {
   restaurants: { restaurants: ['foo'], alert: null },
 };
 const store = mockStore(initialState);
-const props = { setAlert: jest.fn(), getRestaurants: jest.fn() };
+const mockSetAlert = jest.fn();
+const mockGetRestaurants = jest.fn();
+const onSubmit = jest.fn();
 const wrapper = mount(
   <Provider store={store}>
-    <Search props={props} />
+    <Search setAlert={mockSetAlert} getRestaurants={mockGetRestaurants} />
   </Provider>
 );
 
 describe('Search', () => {
-  beforeEach(() => {
+  /* beforeEach(() => {
     const form = wrapper.find('form').first();
     form.simulate('submit', {
       preventDefault: () => {},
     });
-  });
+  }); */
 
   afterEach(() => {
     jest.clearAllMocks();
@@ -40,26 +42,33 @@ describe('Search', () => {
   });
 
   test('3 - setAlert called if search button is pressed with no input', () => {
-    wrapper.find('[data-test="search"]').simulate('click');
-    //expect(store.getActions().length).toBe(1);
-    expect(wrapper.props().children.props.props.setAlert).toHaveBeenCalled();
+    wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+
+    expect(mockSetAlert).toHaveBeenCalled();
   });
 
   test('4 - getRestaurant called when inputs filled and search button clicked ', () => {
     wrapper
       .find('[name="where"]')
+      .at(0)
       .simulate('change', { target: { value: 'foo' } });
 
     wrapper
       .find('[name="what"]')
+      .at(0)
       .simulate('change', { target: { value: 'foo' } });
 
-    wrapper.find('[data-test="best_match"]').simulate('click');
+    wrapper
+      .find('[data-test="best_match"]')
+      .at(0)
+      .simulate('click');
 
-    wrapper.find('[data-test="search"]').simulate('click');
+    wrapper.find('form').simulate('submit', { preventDefault: () => {} });
 
-    expect(
-      wrapper.props().children.props.props.getRestaurants
-    ).toHaveBeenCalled();
+    expect(mockGetRestaurants).toHaveBeenCalledWith({
+      name: 'foo',
+      where: 'foo',
+      sortBy: 'best_match',
+    });
   });
 });
